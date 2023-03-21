5530c1986d6bebec8c6489035ee47eb145ceba6f
Lorena Bastos Amazonas <lorenabastosamz@gmail.com>
2023-03-21 18:00:53 -0400

    application structure update

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,12 @@
+import { Avatar } from './Avatar'
 import { HandsClapping, Trash } from 'phosphor-react';
+
 import styles from './Comment.module.css';
 
 export function Comment() {
     return(
         <div className={styles.comment}>
-            <img src="https://github.com/lorenabstos.png" />
+            <Avatar hasBorder={false} src="https://github.com/lorenabstos.png" />
 
             <div className={styles.commentBox}>
                 <div className={styles.commentContent}>
