960aff96085d2ed0a70982ce436dc0dd221bf25e
Lorena Bastos Amazonas <lorenabastosamz@gmail.com>
2023-03-30 13:37:59 -0400

    useState, declarative prog, keys

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,7 +3,7 @@ import { HandsClapping, Trash } from 'phosphor-react';
 
 import styles from './Comment.module.css';
 
-export function Comment() {
+export function Comment({ content }) {
     return(
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://github.com/lorenabstos.png" />
@@ -22,7 +22,7 @@ export function Comment() {
                             <Trash size={24}/>
                         </button>
                     </header>
-                    <p>Muito bom Devon, parabéns!! 👏👏</p>
+                    <p>{content}</p>
                 </div>
 
                 <footer>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,11 +1,18 @@
 import { format, formatDistanceToNow } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
+import { useState } from 'react';
 
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
 export function Post({ author, publishedAt, content }) {
+    const [comments, setComments] = useState([
+        'post muito bacana, hein?!'
+    ]);
+
+    const [newCommentText, setNewCommentText] = useState('');
+
     const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
         locale: ptBR,
     });
@@ -14,6 +21,19 @@ export function Post({ author, publishedAt, content }) {
         locale: ptBR,
         addSuffix: true,
     });
+
+    function handleCreateNewComment() {
+        event.preventDefault();
+
+        const newCommentText = event.target.comment.value;
+
+        setComments([...comments, newCommentText]);
+        setNewCommentText('');
+    }
+
+    function handleNewCommentChange() {
+        setNewCommentText(event.target.value);
+    }
 
     return (
         <article className={styles.post}>
@@ -33,27 +53,31 @@ export function Post({ author, publishedAt, content }) {
             <div className={styles.content}>
                 {content.map(line => {
                     if (line.type == 'paragraph') {
-                        return <p>{line.content}</p>
+                        return <p key={line.content}>{line.content}</p>
                     } else if (line.type == 'link') {
-                        return <p><a href="#">{line.content}</a></p>
+                        return <p key={line.content}><a href="#">{line.content}</a></p>
                     }
                 })}
             </div>
 
-            <form className={styles.commentForm}>
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
                 <textarea 
-                    placeholder="Escreva um comentário..."
+                    name="comment"
+                    placeholder="Escreva um comentário..." 
+                    value={newCommentText}
+                    onChange={handleNewCommentChange}
                 />
+                
                 <footer>
                     <button type="submit">Publicar</button>
                 </footer>
             </form>
 
             <div className={styles.commentList}>
-                <Comment />
-                <Comment />
-                <Comment />
+                {comments.map(comment => {
+                    return <Comment key={comment} content={comment}/>
+                })}
             </div>
         </article>
     );
